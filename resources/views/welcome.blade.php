8e96570faef86881592b9e477a8958f6a42a17a1
Oege Hiddema <oegehiddema@gmail.com>
2019-06-04 11:36:42 +0200

    create a layout file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <h1>Welcome to my weblog</h1>
-    <a href="/contact">Contact</a> us to learn more.
-</body>
-</html>
+@endsection
+
+
