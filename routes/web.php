d8966e46dc2c6e64305edfe2af2a64203bf207b6
Oege Hiddema <oegehiddema@gmail.com>
2019-06-05 15:35:41 +0200

    use resourceful routing

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
-Route::get('/projects', 'ProjectsController@index');
-Route::get('/projects/create', 'ProjectsController@create');
-Route::post('/projects', 'ProjectsController@store');
+
+// Use resourceful routing
+// Route::get('/projects', 'ProjectsController@index');
+// Route::get('/projects/create', 'ProjectsController@create');
+// Route::post('/projects', 'ProjectsController@store');
+Route::resource('projects','ProjectsController');
