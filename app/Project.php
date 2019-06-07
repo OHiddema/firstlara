6c248b9e948f8960edbca199f2359d315a61ff79
Oege Hiddema <oegehiddema@gmail.com>
2019-06-07 10:52:09 +0200

    create table relation between project and tasks

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $guarded = [];
+
+    // Define relationship between project and tasks
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
 }
