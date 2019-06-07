450472d9b15844db3ecd2aa172d6922ea3338b38
Oege Hiddema <oegehiddema@gmail.com>
2019-06-07 10:58:51 +0200

    display tasks related to project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,4 +5,12 @@
    Description: {{$project->description}}
    <br><br>
    <a href="/projects/{{$project->id}}/edit">Edit project</a>
+
+   <!-- display tasks related to this project -->
+   <br><br>
+   <div>
+      @foreach($project->tasks as $task)
+         <li>{{$task->description}}</li>
+      @endforeach
+   </div>
 @endsection
